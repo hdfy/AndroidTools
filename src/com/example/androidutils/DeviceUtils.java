ab9f3d28ed1a06cc9f406a8a038da4b76e6e5c6a
hdfywxn <1275567243@qq.com>
2015-11-01 00:43:00 +0800

    des:format code date:20151101

--- src/com/example/androidutils/DeviceUtils.java
+++ src/com/example/androidutils/DeviceUtils.java
@@ -6,54 +6,53 @@ import android.text.TextUtils;
 
 public class DeviceUtils {
 
-	/**
-	 * 电信号段:133/153/180/181/189/177； 联通号段:130/131/132/155/156/185/186/145/176；
-	 * 移动号段
-	 * ：134/135/136/137/138/139/150/151/152/157/158/159/182/183/184/187/188/147
-	 * /178。
-	 * 
-	 * @author hdfy
-	 * @param number
-	 * @return
-	 */
-	public static boolean isMobileNumber(String number) {
-		String[] numberHeads = { "133", "153", "180", "181", "189", "177",
-				"130", "131", "132", "155", "156", "185", "186", "145", "176",
-				"134", "135", "136", "137", "138", "139", "150", "151", "152",
-				"157", "158", "159", "182", "183", "184", "187", "188", "147",
-				"178" };
-		if (TextUtils.isEmpty(number)) {
-			return false;
-		}
-		for (String numberHead : numberHeads) {
-			if (number.startsWith(numberHead)) {
-				return true;
-			}
-		}
-		return false;
-	}
+    /**
+     * 电信号段:133/153/180/181/189/177；
+     *  联通号段:130/131/132/155/156/185/186/145/176；
+     * 移动号段：134/135/136/137/138/139/150/151/152/157/158/159/182/183/184/187/188/147/178。
+     * @author hdfy
+     * @param number
+     * @return
+     */
+    public static boolean isMobileNumber(String number) {
+        String[] numberHeads = { 
+                "133", "153", "180", "181", "189",
+                "177", "130", "131", "132", "155",
+                "156", "185", "186", "145", "176",
+                "134","135", "136", "137", "138",
+                "139", "150", "151", "152", "157",
+                "158", "159", "182", "183", "184",
+                "187", "188", "147", "178" };
+        if (TextUtils.isEmpty(number)) {
+            return false;
+        }
+        for (String numberHead : numberHeads) {
+            if (number.startsWith(numberHead)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	/**
-	 * @return deviceID (IMEI)
-	 * 添加权限：android.permission.READ_PHONE_STATE
-	 */
-	public static String getDeviceID(Context context) {
-		TelephonyManager telephonyManager = (TelephonyManager) context
-				.getSystemService(Context.TELEPHONY_SERVICE);
-		String deviceID = telephonyManager.getDeviceId();
-		return deviceID;
-	}
-	
-	/**
-	 * @return phoneNumber
-	 * 添加权限：android.permission.READ_PHONE_STATE
-	 * 也有可能获取不到本机号码（移动运营商没有把手机号码的数据写入到sim卡中）
-	 * 解决办法：给运营商发送短信，然后监听得到此短信，然后分解字符串那么就可以得到手机号了
-	 */
-	public static String getPhoneNumber(Context context) {
-		TelephonyManager telephonyManager = (TelephonyManager) context
-				.getSystemService(Context.TELEPHONY_SERVICE);
-		String phoneNumber = telephonyManager.getLine1Number();//获取本机号码
-		return phoneNumber;
-	}
+    /**
+     * @return deviceID (IMEI)
+     * 添加权限：android.permission.READ_PHONE_STATE
+     */
+    public static String getDeviceID(Context context) {
+        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        String deviceID = telephonyManager.getDeviceId();
+        return deviceID;
+    }
+
+    /**
+     * @return phoneNumber
+     * 添加权限：android.permission.READ_PHONE_STATE
+     * 也有可能获取不到本机号码（移动运营商没有把手机号码的数据写入到sim卡中）
+     * 解决办法：给运营商发送短信，然后监听得到此短信，然后分解字符串那么就可以得到手机号了
+     */
+    public static String getPhoneNumber(Context context) {
+        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        String phoneNumber = telephonyManager.getLine1Number();//获取本机号码
+        return phoneNumber;
+    }
 }
--- src/com/example/androidutils/LogUtil.java
+++ src/com/example/androidutils/LogUtil.java
@@ -8,41 +8,41 @@ import android.util.Log;
  */
 public class LogUtil {
 
-	private static final int VERBOSE = 1;
-	private static final int DEBUG = 2;
-	private static final int INFO = 3;
-	private static final int WARN = 4;
-	private static final int ERROR = 5;
-	private static final int NOTHING = 6;
-	private static final int DEFAULT = VERBOSE; // DEFAULT = NOTHING : print nothing
+    private static final int VERBOSE = 1;
+    private static final int DEBUG = 2;
+    private static final int INFO = 3;
+    private static final int WARN = 4;
+    private static final int ERROR = 5;
+    private static final int NOTHING = 6;
+    private static final int DEFAULT = VERBOSE; // DEFAULT = NOTHING : print nothing
 
-	public static void v(String tag, String msg) {
-		if (DEFAULT <= VERBOSE) {
-			Log.v(tag, msg);
-		}
-	}
+    public static void v(String tag, String msg) {
+        if (DEFAULT <= VERBOSE) {
+            Log.v(tag, msg);
+        }
+    }
 
-	public static void d(String tag, String msg) {
-		if (DEFAULT <= DEBUG) {
-			Log.d(tag, msg);
-		}
-	}
+    public static void d(String tag, String msg) {
+        if (DEFAULT <= DEBUG) {
+            Log.d(tag, msg);
+        }
+    }
 
-	public static void i(String tag, String msg) {
-		if (DEFAULT <= INFO) {
-			Log.i(tag, msg);
-		}
-	}
+    public static void i(String tag, String msg) {
+        if (DEFAULT <= INFO) {
+            Log.i(tag, msg);
+        }
+    }
 
-	public static void w(String tag, String msg) {
-		if (DEFAULT <= WARN) {
-			Log.w(tag, msg);
-		}
-	}
+    public static void w(String tag, String msg) {
+        if (DEFAULT <= WARN) {
+            Log.w(tag, msg);
+        }
+    }
 
-	public static void e(String tag, String msg) {
-		if (DEFAULT <= ERROR) {
-			Log.e(tag, msg);
-		}
-	}
+    public static void e(String tag, String msg) {
+        if (DEFAULT <= ERROR) {
+            Log.e(tag, msg);
+        }
+    }
 }
